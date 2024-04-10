0e44d1dc9196ec3d7af257700c73fd524e1b1d39
Nicoly <nicoly.basso@escola.pr.gov.br>
2024-04-10 07:58:00 -0300

    treinar sequencias logicas condicionais positivo ou negativo apresentar tal mensagem

---
+++ treinar sequencias logicas condicionais .rb
@@ -0,0 +1,17 @@
+treinar sequencias logicas condicionais positivo ou negativo apresentar tal mensagem
+
+
+Algoritmo Armazenar Numero
+   
+    Escrever "Digite um número:"
+    Ler numero
+
+    Se numero > 0 Então
+        A <- numero
+    Senão Se numero < 0 Então
+        B <- numero
+    Fim Se
+
+    Escrever "O número positivo foi armazenado em A:", A
+    Escrever "O número negativo foi armazenado em B:", B
+Fim Algoritmo
